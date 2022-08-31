9f58a75140720316638d84d0891f1c2799607ebe
Omar Bah <omar.bah@elev.ga.ntig.se>
2022-08-31 11:16:36 +0200

    heh

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print (hur många år är du)
-print ("hur många månader är du")
+question = input("who won the 2006 world cup?")
+score=0
+
+if question == "italy":
+    score+=5
+
+question = input("when was the last time juventus won the champions league?")
+
+if question == "1996":
+ score+=5+5
