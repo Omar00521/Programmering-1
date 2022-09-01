a356ec91c8bbb4e2650e5c2ac66e771c56e84c07
Omar Bah <omar.bah@elev.ga.ntig.se>
2022-09-01 10:49:47 +0200

    sport

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-question = input("who won the 2006 world cup?")
-score=0
+from multiprocessing.connection import answer_challenge
 
-if question == "italy":
+
+score=0
+print ("A. Sverige\n B.Italy\n C.England")
+answer1 = input("Who won the 2006 world cup?")
+if answer_challenge == "Italy":
+    score+=3
+
+print ("A.88\ B.100\ C.96")
+anwser2= input("Hur gammal är englands drottning?")
+if  answer_challenge == "96":
+    score+=3
+
+print("A.Dance Monkey\n B.Shape of you\n C. Blinding lights")
+aswser3 = input ("what is the most streamed song on spotify?")
+if answer_challenge == "B":
+  score+=5
+
+print("A.Bayern Munich\n B.Liverpool\n C. Ac Milan\n D.Chelsea ")
+answer4 = input ("what team has second most champions league titles?")
+if answer_challenge == "C":
     score+=5
 
-question = input("when was the last time juventus won the champions league?")
-
-if question == "1996":
- score+=5+5
