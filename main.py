60726baf9a596de7957587b644c1e5195e2c9698
Omar Bah <omar.bah@elev.ga.ntig.se>
2022-09-01 13:21:04 +0200

    dsa

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-from multiprocessing.connection import answer_challenge
-
 
 score=0
 print ("A. Sverige\n B.Italy\n C.England")
 answer1 = input("Who won the 2006 world cup?")
-if answer_challenge == "Italy":
+if answer1 == "Italy" or answer1 == "B":
     score+=3
 
 print ("A.88\ B.100\ C.96")
-anwser2= input("Hur gammal är englands drottning?")
-if  answer_challenge == "96":
+answer2= input("Hur gammal är englands drottning?")
+if  answer2 == "96" or answer2 == "C":
     score+=3
 
 print("A.Dance Monkey\n B.Shape of you\n C. Blinding lights")
-aswser3 = input ("what is the most streamed song on spotify?")
-if answer_challenge == "B":
+answer3 = input ("what is the most streamed song on spotify?")
+if answer3 == "B" or answer3 == "Shape of you":
   score+=5
 
 print("A.Bayern Munich\n B.Liverpool\n C. Ac Milan\n D.Chelsea ")
 answer4 = input ("what team has second most champions league titles?")
-if answer_challenge == "C":
+if answer4 == "C" or answer4 == "ac milan":
     score+=5
 
+print("A.Ferrari\n B.Mercedes\n C.Reb BUll ")
+answer5 
