2f68aab7a96787f096260af33a97c38effd16ac8
Omar00521 <111966597+Omar00521@users.noreply.github.com>
2022-12-16 09:23:19 +0100

    Update fyrairad.py

--- fyrairad.py
+++ fyrairad.py
@@ -2,6 +2,9 @@
 import random
 from secrets import choice
 
+choice_cols=[]
+choice_row=[]
+
 def won(board):
     for row in board:
         if all(x == "X" for x in row):
@@ -15,6 +18,7 @@ def won(board):
             return True
 
 
+#Spelarna väljer sina namn.
 Player1=input("state your name player 1!")
 
 
@@ -24,13 +28,17 @@ position_p1=0#rename
 
 position_p2=0
 
+#Detta printar fram spelplanen.
 def printboard(board):
     for row in board:
         print(*row)
     print("1 2 3 4 5 6 7 ")
 
+#Rader och kolumner
 rows=6
 cols=7
+
+#Spelarna kan då völja vart dem vill placera sina märken inom raderna samt kolumnern.
 board =[["-" for _ in range(cols)]for _ in range(rows)]
 printboard(board)
 play_again= True
@@ -38,8 +46,9 @@ while play_again:
     print(f"Your turn {Player1}")
     choice_row=int(input(f"which row do you want to cross off {Player1}?")) -1
     choice_row=int(input(f"which column do you want to cross off {Player1}?")) -1
-    
-    board[choice_row][Choice_cols] = "X"
+
+    #spelarna tyras om med att markea vart dem vill sätta sina märken för att kunna få fyra i rad.
+    board[choice_row][choice_cols] = "X"
     won(board)
     printboard(board)
 
@@ -47,6 +56,6 @@ while play_again:
     choice_row=int(input(f"which row do you want to cross off {Player2}?")) -1
     choice_row=int(input(f"which column do you want to cross off {Player2}?")) -1
 
-    board[choice_row][Choicecols] = "O"
+    board[choice_row][choice_cols] = "O"
     won(board)
     printboard(board)   
